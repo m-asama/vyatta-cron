7bc0a1664fe7bfb16144cb6675d2039cb3153d00
Stig Thormodsrud <stig@ubnt.com>
2014-06-06 14:58:35 -0700

    Fix bug 696 - task-scheduler: restart script missing
    Signed-off-by: Daniil Baturin <daniil@baturin.org>

--- scripts/vyatta-restart-crond.sh
+++ scripts/vyatta-restart-crond.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if cli-shell-api existsActive service task-scheduler; then
+if cli-shell-api existsActive system task-scheduler; then
    /etc/init.d/cron restart
 else
    echo "Task scheduler is not configured"
